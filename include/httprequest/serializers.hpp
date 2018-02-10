05d5a8ba177c6956667ecc20590cc70ce81df7f1
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-02-10 15:27:04 +0100

    added json serializer

--- include/httprequest/serializers.hpp
+++ include/httprequest/serializers.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstring>
 #include <httprequest/iresponse-delegate.hpp>
+#include <json.hpp>
+
 namespace httprequest {
 
 class TextSerializer : public Serializer<std::string> {
@@ -12,6 +14,23 @@ public:
 
   virtual bool decode(const std::string &in, std::string &out) const {
     out = in;
+    return true;
+  }
+};
+
+class JSONSerializer : public Serializer<nlohmann::json> {
+
+public:
+  virtual bool can(const std::string &mime) const {
+    return std::strncmp(mime.c_str(), "application/json", 16) == 0;
+  }
+
+  virtual bool decode(const std::string &in, nlohmann::json &out) const {
+    try {
+      out = nlohmann::json::parse(in);
+    } catch (...) {
+      return false;
+    }
     return true;
   }
 };
