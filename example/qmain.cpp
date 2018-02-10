ded9105d4715d1a1a983a38bf099f0579a932b8b
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-02-10 16:00:10 +0100

    some cleaning

--- example/qmain.cpp
+++ example/qmain.cpp
@@ -9,24 +9,6 @@ int main(int argc, char **argv) {
 
   QCoreApplication app(argc, argv);
 
-  /*auto channel = std::make_shared<httprequest::QChannel>();
-
-  Client client(channel);
-
-  auto reply = client.request(Request(Method::Get, "http://google.com"));
-
-  reply->once<HeaderEvent>(
-      [](const auto &, auto &) { std::cout << "Header" << std::endl; });
-
-  reply->on<DataEvent>(
-      [](const auto &, auto &) { std::cout << "Data" << std::endl; });
-
-  reply->once<FinishEvent>([reply, &app](const auto &, auto &) {
-    reply->deleteLater();
-    std::cout << "finished" << std::endl;
-    app.exit();
-  });*/
-
   auto channel = std::make_shared<httprequest::QChannel>();
 
   Client client(channel);
