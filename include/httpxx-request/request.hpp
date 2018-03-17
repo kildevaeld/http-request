ca3ae7a4adbd122eddab94dd0c4f4dad72bc5210
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-03-17 01:37:39 +0100

    fix

--- include/httpxx-request/request.hpp
+++ include/httpxx-request/request.hpp
@@ -30,7 +30,7 @@ public:
 
   Request &set_header(const httpxx_types::Header &header);
   Request &set_header(httpxx_types::Header &&header);
-  Request &set_header(const std::string &field, const char *value);
+
   Request &set_header(const std::string &field, const std::string &value);
   Request &set_header(const std::string &field, const char *value);
 
